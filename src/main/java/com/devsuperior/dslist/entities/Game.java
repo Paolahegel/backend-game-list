548efcb6f53fe0668a6be9a2a191eb403daa3427
Paola Moura <paolamoura.kj@hotmail.com>
2025-01-24 05:48:49 -0300

    Seed Game: adicionando tabela de games no banco de dados H2

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -1,6 +1,6 @@
 package com.devsuperior.dslist.entities;
 
-import org.hibernate.engine.jdbc.env.spi.IdentifierCaseStrategy;
+//import org.hibernate.engine.jdbc.env.spi.IdentifierCaseStrategy;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -21,25 +21,29 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String plataform;
+    private String platforms;
     private Double score;
-    private String imUrl;
+    private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
         
     }
 
-    public Game(Long id, String title, Integer year, String genre, String plataform, Double score, String imUrl,
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
             String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.plataform = plataform;
+        this.platforms = platforms;
         this.score = score;
-        this.imUrl = imUrl;
+        this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
     }
@@ -77,11 +81,11 @@ public class Game {
     }
 
     public String getPlataform() {
-        return plataform;
+        return platforms;
     }
 
-    public void setPlataform(String plataform) {
-        this.plataform = plataform;
+    public void setPlataform(String platforms) {
+        this.platforms = platforms;
     }
 
     public Double getScore() {
@@ -92,12 +96,12 @@ public class Game {
         this.score = score;
     }
 
-    public String getImUrl() {
-        return imUrl;
+    public String getImgUrl() {
+        return imgUrl;
     }
 
-    public void setImUrl(String imUrl) {
-        this.imUrl = imUrl;
+    public void setImUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
     }
 
     public String getShortDescription() {
